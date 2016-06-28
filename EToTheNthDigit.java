5ebb0f4b9f58cbd9ec86a9fecf1f6b93787f159c
bbolingit <bbolingit@users.noreply.github.com>
2016-06-28 01:57:38 -0500

    Finished product
    Still could use more testing and comments, but this is a good stopping point

--- EToTheNthDigit.java
+++ EToTheNthDigit.java
@@ -1,6 +1,12 @@
-public class EToNthDigit {
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.lang.Math;
+
+public class EToTheNthDigit {
 
   public static void main(String argv[]){
+
+	int n = 0;
 	double result = 0;
 	boolean notAnswered = true;
 	Scanner input = new Scanner(System.in);//Initalize scanner to read from commandline
@@ -9,16 +15,23 @@ public class EToNthDigit {
 	while(notAnswered){
 		System.out.println("To what decimal place do you want e?");
 		try{
-			//Ateempt to read in an integer after the prompt. Exceptions result in an error message
+			//Attempt to read in an integer after the prompt. Exceptions result in an error message
+
+			n = input.nextInt();
+			if(n > 100 || n < 0) throw new InputMismatchException();//Throws an error message when outside valid input bounds
+
 			place = input.nextInt();
 			if(place > 100 || place < 0) throw new InputMismatchException();//Throws an error message when outside valid input bounds
+
 			notAnswered = false;//If not exceptions are thrown, this is excuted to exit loop
 		} catch (InputMismatchException error) {
 			System.out.println("Please input a valid integer");
 		}
 	}
 	
-	//The actual math. It realies on the 
+	//The actual math. It realies on the power of 10 to induce the decimal places
+	double result = (double) Math.round(((long) Math.pow(10, n)) * Math.E) / Math.pow(10, n);
+
 	result = (double) Math.round((long) Math.pow(10, n)) * Math.E)
 
 	System.out.println(result);
